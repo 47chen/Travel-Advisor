9f71ca218bc49728419d3391d50c349f9be56250
47chen <44563581+47chen@users.noreply.github.com>
2022-02-21 21:42:48 -0500

    env. for API KEY

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,5 @@
 import axios from "axios";
 
-// const options = {
-//   params: {
-//     bl_latitude: "11.847676",
-//     tr_latitude: "12.838442",
-//     bl_longitude: "109.095887",
-//     tr_longitude: "109.149359",
-//   },
-//   headers: {
-//     "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-//     "x-rapidapi-key": "a6340b1f96mshacaecc2abfb183fp1cc0e2jsnc0028fad5fa7",
-//   },
-// };
-
 export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
